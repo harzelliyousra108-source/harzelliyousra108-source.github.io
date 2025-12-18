98ac32e7f57c932aaa28eee9fe118e504b29ca2d
harzelliyousra108-source <harzelliyousra108@gmail.com>
2025-12-19 00:27:52 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -35,7 +35,7 @@ const second = 1000,
   hour = minute * 60,
   day = hour * 24;
 
-let countDown = new Date(`${config.birthdate} 00:00:00`).getTime();
+let countDown = new Date(`${config.birthdate} 01:00:00`).getTime();
 x = setInterval(function() {
   let now = new Date().getTime(),
     distance = countDown - now;
@@ -538,3 +538,4 @@ openLetterBtn.onclick = () => {
   //     console.log("happy birthday");
   // }
 }, second);
+
